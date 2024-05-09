7dd4e869ccfb9a515175c6a3054acf81fb29553e
5meomc <111039846+PlanetaryVision@users.noreply.github.com>
2024-05-08 20:01:02 -0400

    Delete index.d.ts

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import "react";
-    
-declare module 'react' {
-   export interface IframeHTMLAttributes<T> {
-          controls?: 0 | 1
-          fs?: 0 | 1
-          hl?: string
-        }
-    }
